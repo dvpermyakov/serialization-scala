4cb52c49e9cf92df8c41d93e3865d3894aa9370d
dmitrii.permiakov <dmitrii.permiakov@revolut.com>
2020-06-21 16:42:24 +0300

    add ClassFactory

--- src/main/scala/com/dvpermyakov/serialization/ClassFactory.scala
+++ src/main/scala/com/dvpermyakov/serialization/ClassFactory.scala
@@ -6,10 +6,39 @@ abstract class ClassFactory[T] {
 
 object ClassFactory {
   def get[T](rawType: Class[T]): ClassFactory[T] = {
-    () => {
-      val constructor = rawType.getDeclaredConstructor()
-      constructor.setAccessible(true)
-      constructor.newInstance()
+    new ClassFactory[T] {
+      override def newInstance(): T = {
+        try {
+          return getByDefaultConstructor(rawType)
+        } catch {
+          case _: Throwable =>
+        }
+
+        try {
+          return getByUnsafe(rawType)
+        } catch {
+          case _: Throwable =>
+        }
+
+        throw new IllegalStateException("Didn't find new instance method")
+      }
     }
   }
+
+  private def getByDefaultConstructor[T](rawType: Class[T]): T = {
+    val constructor = rawType.getDeclaredConstructor()
+    constructor.setAccessible(true)
+    constructor.newInstance()
+  }
+
+  private def getByUnsafe[T](rawType: Class[T]): T = {
+    val unsafeClass = Class.forName("sun.misc.Unsafe")
+    val f = unsafeClass.getDeclaredField("theUnsafe")
+    f.setAccessible(true)
+    val unsafe = f.get(null)
+    val allocateInstance = unsafeClass.getMethod("allocateInstance", classOf[Class[_]])
+    val obj = allocateInstance.invoke(unsafe, rawType)
+    obj.asInstanceOf[T]
+  }
+
 }
--- src/test/scala/com/dvpermyakov/serialization/SerializationTest.scala
+++ src/test/scala/com/dvpermyakov/serialization/SerializationTest.scala
@@ -2,27 +2,51 @@ package com.dvpermyakov.serialization
 
 import org.junit.{Assert, Test}
 
-class Sample() {
+class SampleDefaultConstructor() {
   val valueString: String = "str"
-  val valueInt: Int = 0
-  val valueLong: Long = 0L
-  val valueFloat: Float = 0f
-  val valueDouble: Double = 0
-  val valueChar: Char = '0'
-  val valueBoolean: Boolean = false
-  val valueByte: Byte = 0
-  val valueShort: Short = 0
+  val valueInt: Int = 100
+  val valueLong: Long = 110L
+  val valueFloat: Float = 120f
+  val valueDouble: Double = 130
+  val valueChar: Char = '$'
+  val valueBoolean: Boolean = true
+  val valueByte: Byte = 10
+  val valueShort: Short = 20
 }
+
+class SampleConstructor(
+                         val valueString: String,
+                         val valueInt: Int,
+                         val valueLong: Long,
+                         val valueFloat: Float,
+                         val valueDouble: Double,
+                         val valueChar: Char,
+                         val valueBoolean: Boolean,
+                         val valueByte: Byte,
+                         val valueShort: Short
+                       )
+
+case class SampleCase(
+                       valueString: String,
+                       valueInt: Int,
+                       valueLong: Long,
+                       valueFloat: Float,
+                       valueDouble: Double,
+                       valueChar: Char,
+                       valueBoolean: Boolean,
+                       valueByte: Byte,
+                       valueShort: Short
+                     )
 
 class SerializationTest {
 
   @Test
-  def simpleClass(): Unit = {
-    val serialization = new Serialization(ClassFactory.get(classOf[Sample]))
+  def sampleDefaultConstructor(): Unit = {
+    val serialization = new Serialization(ClassFactory.get(classOf[SampleDefaultConstructor]))
 
-    val sample = new Sample()
+    val sample = new SampleDefaultConstructor()
     val serData = serialization.serialize(sample)
-    val sample2: Sample = serialization.deserialize(serData)
+    val sample2: SampleDefaultConstructor = serialization.deserialize(serData)
 
     Assert.assertEquals(
       sample.valueString,
@@ -64,4 +88,119 @@ class SerializationTest {
     )
   }
 
+  @Test
+  def sampleConstructor(): Unit = {
+    val serialization = new Serialization(ClassFactory.get(classOf[SampleConstructor]))
+
+    val sample = new SampleConstructor(
+      valueString = "new string",
+      valueInt = 10,
+      valueBoolean = false,
+      valueByte = 100,
+      valueChar = '*',
+      valueDouble = 200,
+      valueLong = 300L,
+      valueFloat = 400f,
+      valueShort = 20
+    )
+    val serData = serialization.serialize(sample)
+    val sample2: SampleConstructor = serialization.deserialize(serData)
+
+    Assert.assertEquals(
+      sample.valueString,
+      sample2.valueString
+    )
+    Assert.assertEquals(
+      sample.valueInt,
+      sample2.valueInt
+    )
+    Assert.assertEquals(
+      sample.valueLong,
+      sample2.valueLong
+    )
+    Assert.assertEquals(
+      sample.valueFloat,
+      sample2.valueFloat,
+      0.000001f
+    )
+    Assert.assertEquals(
+      sample.valueDouble,
+      sample2.valueDouble,
+      0.000001
+    )
+    Assert.assertEquals(
+      sample.valueChar,
+      sample2.valueChar
+    )
+    Assert.assertEquals(
+      sample.valueBoolean,
+      sample2.valueBoolean
+    )
+    Assert.assertEquals(
+      sample.valueByte,
+      sample2.valueByte
+    )
+    Assert.assertEquals(
+      sample.valueShort,
+      sample2.valueShort
+    )
+  }
+
+  @Test
+  def sampleCase(): Unit = {
+    val serialization = new Serialization(ClassFactory.get(classOf[SampleCase]))
+
+    val sample = SampleCase(
+      valueString = "str",
+      valueInt = 400,
+      valueBoolean = true,
+      valueByte = 121,
+      valueChar = '0',
+      valueDouble = 111,
+      valueLong = 112L,
+      valueFloat = 122f,
+      valueShort = 30
+    )
+    val serData = serialization.serialize(sample)
+    val sample2: SampleCase = serialization.deserialize(serData)
+
+    Assert.assertEquals(
+      sample.valueString,
+      sample2.valueString
+    )
+    Assert.assertEquals(
+      sample.valueInt,
+      sample2.valueInt
+    )
+    Assert.assertEquals(
+      sample.valueLong,
+      sample2.valueLong
+    )
+    Assert.assertEquals(
+      sample.valueFloat,
+      sample2.valueFloat,
+      0.000001f
+    )
+    Assert.assertEquals(
+      sample.valueDouble,
+      sample2.valueDouble,
+      0.000001
+    )
+    Assert.assertEquals(
+      sample.valueChar,
+      sample2.valueChar
+    )
+    Assert.assertEquals(
+      sample.valueBoolean,
+      sample2.valueBoolean
+    )
+    Assert.assertEquals(
+      sample.valueByte,
+      sample2.valueByte
+    )
+    Assert.assertEquals(
+      sample.valueShort,
+      sample2.valueShort
+    )
+  }
 }
